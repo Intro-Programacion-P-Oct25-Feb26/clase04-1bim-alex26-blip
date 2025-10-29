3abbea0f5ef788f9b93aa3d933a3f6d23c3c33ca
alex bravo <fabriciobravo373@gmail.com>
2025-10-29 12:48:41 -0500

    grupo9

---
+++ Ejemplo01/src/ejemplo01/Ejemplo061.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejemplo01;
+
+/**
+ *
+ * @author reroes
+ */
+public class Ejemplo061 {
+    public static void main(String[] args) {
+        // Uso de printf
+        // f: formato
+        
+        String nombre = "Alex Bravo";
+        String apellido = "Bravo Celi";
+        int nacimiento = 2006;
+        String direccion = "san cayetano bajo";
+        String universidad = "utpl";
+        String ciclo = "primer cliclo";
+        String asignatura = "introduccion a la programacion";
+        String paralelo = "a";
+                
+        
+        // System.out.println(nombreEstudiante+" "+apellidoEstudiante+ " "+ 
+        // nacimiento);
+        // En printf; 
+        // usamos %s para reemplazar las cadenas, enteros, decimales
+        // usamos %d para reemplazar los valores enteros
+        // usamos %f para reemplazar los valore decimales
+        // System.out.printf("%s\n\n%s\n\n%s\n", nombreEstudiante, 
+        //        apellidoEstudiante, nacimiento);
+        System.out.printf("Nombre:\n\t%s\nApellido:\t%s",nombre,apellido);
+        
+     
+    }
+}
